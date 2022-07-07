75ab759775a91679c922ccf0e58cbe4549cf67bf
unknown <abhisheksrivastava9719@gmail.com>
2022-07-07 15:22:52 +0530

    [Abhishek] added printing of different permuataions of a string function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, to the algorithm and data structure problems");
+﻿using Algorithms_DataStructures;
+Console.WriteLine("Hello, to the algorithm and data structure problems");
+Console.WriteLine("Enter a word whose anagram you want");
+String s=Console.ReadLine();
+question.anagram("", s);
---
+++ question.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStructures
+{
+    internal class question
+    {
+        public static void anagram(String fix, String s)
+        {
+            int l = s.Length;
+            if (l == 1)
+            {
+                Console.WriteLine(fix + s);
+            }
+            else
+            {
+                for (int i = 0; i < l; i++)
+                {
+                    char[] mychar = s.ToCharArray();
+                    char c = mychar[i];
+                    String st = s.Substring(0, i) + s.Substring(i + 1);
+                    anagram(fix + c, st);
+                }
+            }
+        }
+    }
+}
