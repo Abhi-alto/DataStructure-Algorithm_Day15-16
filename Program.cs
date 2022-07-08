5ee983bc8262b7160edfa3b95e22d521911d837a
unknown <abhisheksrivastava9719@gmail.com>
2022-07-08 11:59:38 +0530

    [Abhishek] added prime number, palindrome number and anagram functionality between range 0 -1000

--- Program.cs
+++ Program.cs
@@ -3,5 +3,8 @@ Console.WriteLine("Hello, to the algorithm and data structure problems");
 Console.WriteLine("Prime numbers between range 0-1000 are -");
 for(int x=2;x<=1000;x++)
 {
-    question.prime(x);
+    if(question.prime(x) == true && question.palindrome(x)==true)
+    {
+        Console.WriteLine(x);
+    }
 }
